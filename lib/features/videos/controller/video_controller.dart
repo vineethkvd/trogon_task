c426a22682e1a57d1f38bb17020ba7dd111466c0
Vineeth <67091853+vineethkvd@users.noreply.github.com>
2024-12-05 19:13:59 +0530

    done

--- lib/features/videos/controller/video_controller.dart
+++ lib/features/videos/controller/video_controller.dart
@@ -1,44 +1,103 @@
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import '../../../core/utils/shared/component/widgets/custom_toast.dart';
 import '../model/video_model.dart';
+import '../../../core/utils/shared/component/widgets/custom_toast.dart';
 import '../repository/module_repository.dart';
-
+import 'package:video_player/video_player.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:chewie/chewie.dart';
+import 'package:flutter/material.dart';
 class VideoController extends ChangeNotifier {
   final _api = ModuleRepository();
   bool isLoading = false;
-  String vidUrl = '';
-  String curTitle = '';
-  String curDes = '';
-  var moduleModel = VideoModel();
-  var moduleList = <VideoModel>[];
+  bool isPlayerReady = false;
+
+  String currentTitle = '';
+  String currentDescription = '';
+  String currentVideoType = '';
+  List<VideoModel> moduleList = [];
+  late YoutubePlayerController ytController;
+  late VideoPlayerController videoPlayerController;
+  ChewieController? chewieController;
+
   Future<void> fetchModules({required String moduleId}) async {
     try {
       isLoading = true;
-      notifyListeners(); // Notify the UI that loading is in progress.
+      notifyListeners();
 
       final response = await _api.moduleApi(moduleId: moduleId);
       if (response != null && response['status'] == 200) {
         final List<dynamic> responseData = response['data'];
-        moduleList.clear(); // Clear old data.
-        moduleList.addAll(
-          responseData.map((json) => VideoModel.fromJson(json)).toList(),
-        );
+        moduleList = responseData
+            .map((json) => VideoModel.fromJson(json))
+            .toList();
+
         if (moduleList.isNotEmpty) {
-          vidUrl = moduleList.first.videoUrl.toString();
-          curTitle = moduleList.first.title.toString();
-          curDes = moduleList.first.description.toString();
+          initializePlayer(moduleList.first);
         }
       } else {
         CustomToast.showCustomToast(message: "Unexpected error occurred");
       }
     } catch (e) {
       if (kDebugMode) {
-        print("Error $e");
+        print("Error fetching modules: $e");
       }
     } finally {
       isLoading = false;
-      notifyListeners(); // Notify the UI to rebuild with updated data.
+      notifyListeners();
     }
+  }
+
+  void initializePlayer(VideoModel video) {
+    isPlayerReady = true;
+    notifyListeners();
+
+    currentTitle = video.title ?? '';
+    currentDescription = video.description ?? '';
+    currentVideoType = video.videoType ?? '';
+
+    if (video.videoType == 'YouTube') {
+      initializeYouTubePlayer(video.videoUrl ?? '');
+    } else {
+      initializeChewiePlayer(video.videoUrl ?? '');
+    }
+  }
+
+  void initializeYouTubePlayer(String url) {
+    final videoId = YoutubePlayer.convertUrlToId("https://www.youtube.com/watch?v=7AcwQ4JoPNg") ?? '';
+    ytController = YoutubePlayerController(
+      initialVideoId: videoId,
+      flags: const YoutubePlayerFlags(autoPlay: false, controlsVisibleAtStart: true),
+    );
+
+    isPlayerReady = true;
+    notifyListeners();
+  }
+
+  Future<void> initializeChewiePlayer(String url) async {
+    videoPlayerController = VideoPlayerController.network(url);
+    try {
+      await videoPlayerController.initialize();
+      chewieController = ChewieController(
+        videoPlayerController: videoPlayerController,
+        autoInitialize: true,
+        autoPlay: false,
+        showControls: true,
+        placeholder: Container(color: Colors.black),
+      );
+
+      isPlayerReady = true;
+    } catch (e) {
+      CustomToast.showCustomToast(message: "Error initializing video: $e");
+    } finally {
+      notifyListeners();
+    }
+  }
+
+  @override
+  void dispose() {
+    ytController.dispose();
+    videoPlayerController.dispose();
+    chewieController?.dispose();
+    super.dispose();
   }
 }
--- lib/features/videos/view/video_page.dart
+++ lib/features/videos/view/video_page.dart
@@ -1,168 +1,56 @@
-import 'package:cached_network_image/cached_network_image.dart';
-import 'package:chewie/chewie.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:read_more_text/read_more_text.dart';
 import 'package:shimmer/shimmer.dart';
-import 'package:video_player/video_player.dart';
-// import 'package:youtube_player_flutter/youtube_player_flutter.dart';
-
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:chewie/chewie.dart';
 import '../../../core/utils/config/styles/colors.dart';
-import '../../../core/utils/shared/component/widgets/custom_toast.dart';
 import '../controller/video_controller.dart';
-import '../model/video_model.dart';
 
-class VideoPage extends StatefulWidget {
+class VideoPage extends StatelessWidget {
   final String moduleId;
 
   const VideoPage({Key? key, required this.moduleId}) : super(key: key);
 
   @override
-  State<VideoPage> createState() => _VideoPageState();
-}
-
-class _VideoPageState extends State<VideoPage> {
-  // late YoutubePlayerController _ytController;
-  late VideoPlayerController _videoPlayerController;
-  ChewieController? _chewieController;
-  bool _isPlayerReady = false;
-  String selectedLessonTitle = '';
-  String selectedLessonDescription = '';
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _loadModules();
-    });
-  }
-
-  Future<void> _loadModules() async {
-    final videoController = Provider.of<VideoController>(context, listen: false);
-    await videoController.fetchModules(moduleId: widget.moduleId);
-
-    if (videoController.moduleList.isNotEmpty) {
-      final firstLesson = videoController.moduleList.first;
-
-      switch (firstLesson.videoType) {
-        case "YouTube":
-          // initializeYouTubePlayer(
-          //   url: firstLesson.videoUrl ?? '',
-          //   title: firstLesson.title ?? '',
-          //   description: firstLesson.description ?? '',
-          // );
-          break;
-        case "Vimeo":
-        // Handle Vimeo case here if needed
-          break;
-        default:
-          initializeChewiePlayer(
-            url: firstLesson.videoUrl ?? '',
-            title: firstLesson.title ?? '',
-            description: firstLesson.description ?? '',
-          );
-          break;
-      }
-    }
-  }
-
-  void initializeYouTubePlayer({
-    required String url,
-    required String title,
-    required String description,
-  }) {
-    // final videoId = YoutubePlayer.convertUrlToId(url) ?? '';
-    // _ytController = YoutubePlayerController(
-    //   initialVideoId: videoId,
-    //   flags: const YoutubePlayerFlags(
-    //     autoPlay: false,
-    //     controlsVisibleAtStart: true,
-    //   ),
-    // );
-    //
-    // setState(() {
-    //   _isPlayerReady = true;
-    //   selectedLessonTitle = title;
-    //   selectedLessonDescription = description;
-    // });
-  }
-
-  Future<void> initializeChewiePlayer({
-    required String url,
-    required String title,
-    required String description,
-  }) async {
-    _videoPlayerController = VideoPlayerController. networkUrl (Uri.parse(url));
-
-    try {
-      await _videoPlayerController.initialize();
-    } catch (error) {
-      CustomToast.showCustomToast(message: "Error loading video: $error");
-      return;
-    }
-
-    setState(() {
-      _isPlayerReady = true;
-      selectedLessonTitle = title;
-      selectedLessonDescription = description;
-    });
-
-    _chewieController = ChewieController(
-      videoPlayerController: _videoPlayerController,
-      autoInitialize: true,
-      autoPlay: false,
-      showControls: true,
-      placeholder: Container(color: Colors.black),
-    );
-  }
-
-  @override
-  void dispose() {
-    _videoPlayerController.dispose();
-    _chewieController?.dispose();
-    // _ytController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
-    final videoController = Provider.of<VideoController>(context);
-    final width = MediaQuery.of(context).size.width;
-
-    return Scaffold(
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        leading: IconButton(
-          onPressed: () => Navigator.of(context).pop(),
-          icon: const Icon(Icons.arrow_back, color: Colors.white),
-        ),
-        elevation: 0,
-        backgroundColor: AppColor.appBarColor,
-        centerTitle: true,
-        title: const Text(
-          "Lessons",
-          style: TextStyle(
-            fontSize: 18,
-            fontWeight: FontWeight.bold,
-            color: Colors.white,
+    return ChangeNotifierProvider(
+      create: (_) => VideoController()..fetchModules(moduleId: moduleId),
+      child: Scaffold(
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.arrow_back, color: Colors.white),
+            onPressed: () => Navigator.pop(context),
           ),
+          backgroundColor: AppColor.appBarColor,
+          title: const Text(
+            "Video Lessons",
+            style: TextStyle(color: Colors.white, fontSize: 18),
+          ),
+          centerTitle: true,
         ),
-      ),
-      body: videoController.isLoading
-          ? _buildShimmerEffect()
-          : videoController.moduleList.isEmpty
-          ? const Center(
-        child: Text(
-          "No lessons available",
-          style: TextStyle(fontSize: 16, color: Colors.grey),
+        body: Consumer<VideoController>(
+          builder: (context, videoController, child) {
+            if (videoController.isLoading) {
+              return _buildShimmerEffect();
+            }
+
+            if (videoController.moduleList.isEmpty) {
+              return const Center(
+                child: Text("No lessons available", style: TextStyle(color: Colors.grey, fontSize: 16)),
+              );
+            }
+
+            return Container(
+              color: Colors.white,
+              child: Column(
+                children: [
+                  _buildVideoPlayer(videoController), // The video player section
+                  Expanded(child: _buildLessonList(videoController)), // The suggestions list
+                ],
+              ),
+            );
+          },
         ),
-      )
-          : Column(
-        children: [
-          if (_isPlayerReady) buildVideoPlayer(width),
-          Expanded(child: buildLessonList(videoController.moduleList)),
-        ],
       ),
     );
   }
@@ -178,133 +66,91 @@ class _VideoPageState extends State<VideoPage> {
     );
   }
 
-  Widget buildVideoPlayer(double width) {
+  Widget _buildVideoPlayer(VideoController videoController) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         SizedBox(
-          width: width,
           height: 200,
-          child: _isPlayerReady
-              ? (_chewieController != null
-              ? Chewie(controller: _chewieController!):SizedBox.shrink())
-              // : YoutubePlayer(controller: _ytController))
+          width: double.infinity,
+          child: videoController.isPlayerReady
+              ? (videoController.currentVideoType == 'YouTube'
+              ? YoutubePlayer(controller: videoController.ytController)
+              : Chewie(controller: videoController.chewieController!))
               : _buildShimmerEffect(),
         ),
         Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0),
           child: Text(
-            selectedLessonTitle,
+            videoController.currentTitle,
             style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
           ),
         ),
         Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
           child: Text(
-            selectedLessonDescription,
-            style: const TextStyle(fontSize: 16),
+            videoController.currentDescription,
+            style: const TextStyle(fontSize: 16, color: Colors.grey),
           ),
         ),
+        const Divider(thickness: 1, color: Colors.grey),
       ],
     );
   }
 
-  Widget buildLessonList(List<VideoModel> lessons) {
+  Widget _buildLessonList(VideoController videoController) {
     return ListView.builder(
-      itemCount: lessons.length,
+      itemCount: videoController.moduleList.length,
       itemBuilder: (context, index) {
-        final lesson = lessons[index];
+        final lesson = videoController.moduleList[index];
         return InkWell(
           onTap: () {
             if (lesson.videoUrl != null) {
-              if (lesson.videoType == 'YouTube') {
-                initializeYouTubePlayer(
-                  url: lesson.videoUrl!,
-                  title: lesson.title ?? '',
-                  description: lesson.description ?? '',
-                );
-              } else {
-                initializeChewiePlayer(
-                  url: lesson.videoUrl!,
-                  title: lesson.title ?? '',
-                  description: lesson.description ?? '',
-                );
-              }
+              videoController.initializePlayer(lesson);
             } else {
-              CustomToast.showCustomToast(message: "Video URL not available");
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text("Video URL not available")),
+              );
             }
           },
-          child: _buildLessonCard(lesson),
-        );
-      },
-    );
-  }
-
-  Widget _buildLessonCard(VideoModel lesson) {
-    return Container(
-      margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(8.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.3),
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: const Offset(0, 3),
-          ),
-        ],
-      ),
-      child: Row(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: SizedBox(
-              width: 60,
-              height: 60,
-              child: Stack(
-                children: [
-                  Positioned.fill(
-                    child: CachedNetworkImage(
-                      imageUrl: '', // Use actual image URL here
-                      fit: BoxFit.fill,
-                      placeholder: (context, url) => Container(color: Colors.grey),
-                      errorWidget: (context, url, error) =>
-                          Container(color: Colors.grey),
-                    ),
-                  ),
-                  const Center(
-                    child: Icon(Icons.play_circle_fill, color: Colors.white, size: 30),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const SizedBox(width: 16),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+          child: Container(
+            margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+            child: Row(
               children: [
-                Text(
-                  lesson.title ?? '',
-                  style: const TextStyle(
-                      fontSize: 16, fontWeight: FontWeight.bold),
-                  maxLines: 2,
-                  overflow: TextOverflow.ellipsis,
+                // Thumbnail image
+                Container(
+                  width: 80,
+                  height: 60,
+                  color: Colors.grey[300], // Placeholder background
+                  child: const Icon(Icons.play_circle_fill, color: Colors.white),
                 ),
-                const SizedBox(height: 8),
-                ReadMoreText(
-                  lesson.description ?? '',
-                  numLines: 1,
-                  readMoreText: 'Read more',
-                  readLessText: 'Read less',
+                const SizedBox(width: 10),
+                // Title and description
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        lesson.title ?? '',
+                        style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                        maxLines: 2,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                      const SizedBox(height: 4),
+                      Text(
+                        lesson.description ?? '',
+                        style: const TextStyle(fontSize: 14, color: Colors.grey),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
